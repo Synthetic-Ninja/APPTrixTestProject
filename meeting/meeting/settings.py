43673164a6545e296e2deab814decf0e814db206
Synthetic-Ninja <sinyagin.timofey@yandex.ru>
2023-07-01 12:42:31 +0300

    Создал приложение clients

--- meeting/meeting/settings.py
+++ meeting/meeting/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
+    # Мои приложения
+    'clients.apps.ClientsConfig'
 ]
 
 MIDDLEWARE = [
