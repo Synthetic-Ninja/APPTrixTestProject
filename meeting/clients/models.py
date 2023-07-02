01058b1b04342e4826f1cd0bfaf260ca3c711c0a
Synthetic-Ninja <sinyagin.timofey@yandex.ru>
2023-07-02 16:37:04 +0300

    Создал модель пользователя и сервис для обработки изображений

--- meeting/clients/models.py
+++ meeting/clients/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+from services.image_service import WatermarkImage
+
+
+class ClientSex(models.Model):
+    """Модель пола пользователей"""
+    sex_name = models.TextField(null=False)
+
+
+class Client(AbstractUser):
+    """Модель пользователя"""
+
+    first_name = models.TextField(null=False)
+    last_name = models.TextField(null=False)
+    sex = models.ForeignKey(to=ClientSex, on_delete=models.CASCADE)
+    email = models.EmailField(null=False, unique=True)
+    avatar = models.ImageField(upload_to='users_avatars', null=True, blank=True,
+                               default='default/default_avatar.png')
+
+    position_latitude = models.FloatField(validators=(MaxValueValidator(90), MinValueValidator(-90)))
+    position_longitude = models.FloatField(validators=(MaxValueValidator(180), MinValueValidator(-180)))
+
+    def save(self, *args, **kwargs):
+        if self.avatar:
+            self.avatar.file = WatermarkImage(self.avatar.file).get()
+
+        return super().save(*args, **kwargs)
---
+++ meeting/services/image_service.py
@@ -0,0 +1,37 @@
+import io
+
+from PIL import Image
+from django.core.files.base import File
+
+from meeting.settings import STATICFILES_DIRS
+
+
+class WatermarkImage:
+    """Класс для добавления вотермарки на входную фотографию """
+
+    WATERMARK_PATH = f'{STATICFILES_DIRS[0]}/images/watermark.png'
+
+    def __init__(self, image: File, width=300, height=300):
+        # Открываем изображения и приводим к нужному размеру
+        self.image_width = width
+        self.image_height = height
+        self.image = Image.open(image).resize((width, height)).convert('RGB')
+
+    def __add_water_mark(self, width=200, height=200) -> None:
+        """Функция добавляет вотермарку в self.image"""
+
+        # Открываем изображение вотермарки и приводим к нужному размеру
+        with Image.open(self.WATERMARK_PATH).resize((width, height)) as watermark:
+            # Считаем координаты, чтобы вотермарка была по центру
+            paste_coords = ((self.image.width - width) // 2,
+                            (self.image.height - height) // 2)
+            self.image.paste(watermark, paste_coords, watermark)
+
+    def get(self) -> File:
+        """Функция возвращает объект FIle, который содержит изобржаение с вотермаркой"""
+
+        self.__add_water_mark()
+        new_image = io.BytesIO()
+        self.image.save(new_image, format='JPEG')
+        self.image.close()
+        return File(new_image)
